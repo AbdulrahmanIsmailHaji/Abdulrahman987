a36bb129c74630d19a3393e86278339ea80d0a25
Abdulrahman Ismail <abdulrahmanismail987@gmail.com>
2024-01-12 21:38:39 +0300

    abdulrahman987

---
+++ src/components/Circle.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const CountdownTimer = ({ fillPercentage, text, per }) => {
+  const radius = 50;
+  const circumference = 2 * Math.PI * radius;
+  const strokeDashoffset = (fillPercentage / 100) * circumference;
+
+  return (
+    <div className="tst-timer ">
+      <div className="countdown-container">
+        <svg
+          viewBox="0 0 110 110"
+          width="110"
+          height="110"
+          className="u-w-auto u-absolute"
+        >
+          <circle
+            className="countdown-circle"
+            cx="55"
+            cy="55"
+            r={radius}
+            fill="none"
+            stroke="url(#gradient)"
+            strokeWidth="6"
+            strokeDasharray={`${circumference}px`}
+            style={{
+              transition: "all 1s linear",
+              strokeDashoffset: `${strokeDashoffset}px`,
+              transform: "rotate(-90deg)",
+              transformOrigin: "center",
+            }}
+          />
+          <defs>
+            <linearGradient id="gradient" gradientTransform="rotate(90)">
+              <stop
+                offset="0%"
+                style={{ stopColor: "rgba(109, 195, 118, 1)" }}
+              />
+              <stop offset="35%" style={{ stopColor: "rgba(8, 28, 113, 1)" }} />
+              <stop offset="100%" style={{ stopColor: "rgba(255, 0, 1, 1)" }} />
+            </linearGradient>
+          </defs>
+        </svg>
+        <h5 className="countdown-label">{text}</h5>
+        <h5 className="countdown-per">{per}%</h5>
+      </div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,64 +1,79 @@
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import colorSharp from "../assets/img/color-sharp.png";
+import Circle from "./Circle";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 5,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
 
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                        </Carousel>
-                    </div>
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>
+                I am proficient in a wide range of technologies, showcasing a
+                robust skill set that includes expertise in JavaScript and
+                TypeScript for building dynamic and efficient web applications.
+                My proficiency extends to front-end development with React.js,
+                enabling me to create interactive and user-friendly interfaces.
+                On the server side, I am skilled in utilizing Node.js and
+                Express.js to build scalable and performant back-end solutions.
+                Additionally, my experience extends to the powerful Next.js
+                framework for server-rendered React applications. I am
+                well-versed in database management, particularly with MongoDB,
+                and I possess a strong command of styling languages such as CSS,
+                Tailwind CSS, and Sass. With a keen eye for design and a solid
+                foundation in HTML, I am adept at crafting visually appealing
+                and responsive web pages.
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="owl-carousel owl-theme skill-slider"
+              >
+                <Circle fillPercentage={10} text="HTML" per={90} />
+                <Circle fillPercentage={10} text="CSS" per={90} />
+                <Circle fillPercentage={15} text="JAVASCRIPT" per={85} />
+                <Circle fillPercentage={25} text="TYPESCRIPT" per={75} />
+                <Circle fillPercentage={15} text="REACT" per={85} />
+                <Circle fillPercentage={35} text="NODE.JS " per={65} />
+                <Circle fillPercentage={35} text="EXPRESS.JS" per={65} />
+                <Circle fillPercentage={15} text="NEXT.JS" per={85} />
+                <Circle fillPercentage={10} text="MONGODB" per={90} />
+                <Circle fillPercentage={15} text="TAILWINDCSS" per={85} />
+                <Circle fillPercentage={10} text="SASS" per={80} />
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
-  )
-}
+  );
+};
