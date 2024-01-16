9ab884fd965a789d6b0ee649412c8e3f8e1c075d
Abdulrahman Ismail <abdulrahmanismail987@gmail.com>
2024-01-16 20:05:24 +0300

    Update My Portfolio

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,61 @@
+import React from "react";
 import { Col } from "react-bootstrap";
+import { FaReact, FaJs } from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiRedux,
+  SiAxios,
+  SiChartdotjs,
+  SiMongodb,
+} from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+const getIcon = (iconName) => {
+  switch (iconName) {
+    case "react":
+      return <FaReact className="icon react-icon" />;
+    case "js":
+      return <FaJs className="icon js-icon" />;
+    case "tailwind":
+      return <SiTailwindcss className="icon tail-icon" />;
+    case "redux":
+      return <SiRedux className="icon red-icon" />;
+    case "axios":
+      return <SiAxios className="icon axios-icon" />;
+    case "chartjs":
+      return <SiChartdotjs className="icon char-icon" />;
+    case "nextjs":
+      return <TbBrandNextjs className="icon next-icon" />;
+    case "mongo":
+      return <SiMongodb className="icon mongo-icon" />;
+    default:
+      return null;
+  }
+};
+
+export const ProjectCard = ({ title, description, imgUrl, link, icons }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+      <a
+        style={{ color: "white" }}
+        className="project-card-a"
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="proj-imgbx">
+          <img src={imgUrl} alt="" />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+            <div className="project-icons">
+              {icons.map((icon, index) => (
+                <React.Fragment key={index}>{getIcon(icon)}</React.Fragment>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      </a>
     </Col>
-  )
-}
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,42 +3,66 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
+  const projectsTab1 = [
+    {
+      title: "CryptoVerse",
+      description: "Working with api",
+      imgUrl: projImg1,
+      link: "https://cryptoverse-puce.vercel.app/",
+      icons: ["react", "redux", "axios", "chartjs"],
+    },
+    {
+      title: "Typing Speed App",
+      description: "Using Next js and NextAuth for Authentication",
+      imgUrl: projImg2,
+      link: "https://typing-application-delta.vercel.app/",
+      icons: ["nextjs", "mongo", "axios"],
+    },
+    {
+      title: "E-Commerce",
+      description: "Using Plain React",
+      imgUrl: projImg3,
+      link: "https://e-commerce-black-two.vercel.app/",
+      icons: ["react"],
+    },
+    {
+      title: "Promptopia",
+      description: "Design & Development",
+      imgUrl: projImg4,
+      link: "https://promtopia-ai.vercel.app/",
+      icons: ["nextjs", "mongo"],
+    },
+    {
+      title: "GPT-3",
+      description: "Design & Development",
+      imgUrl: projImg5,
+      link: "https://gpt-3-chi-ebon.vercel.app/",
+      icons: ["react"],
+    },
+  ];
 
-  const projects = [
+  const projectsTab2 = [
     {
-      title: "Business Startup",
+      title: "Promptopia",
       description: "Design & Development",
-      imgUrl: projImg1,
+      imgUrl: projImg4,
+      link: "https://promtopia-ai.vercel.app/",
+      icons: ["nextjs", "mongo"],
     },
     {
-      title: "Business Startup",
+      title: "GPT-3",
       description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      imgUrl: projImg5,
+      link: "https://gpt-3-chi-ebon.vercel.app/",
+      icons: ["react"],
     },
   ];
 
@@ -48,51 +72,92 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    I have developed a multitude of projects employing a
+                    comprehensive technological stack, demonstrating proficiency
+                    in both front-end and back-end domains. My front-end
+                    expertise encompasses the utilization of HTML, CSS, SASS,
+                    Tailwind CSS, and JavaScript, complemented by frameworks
+                    like React and Next.js for building dynamic and responsive
+                    user interfaces. On the server side, I have leveraged the
+                    power of Node.js and Express to ensure robust and scalable
+                    back-end functionalities. In terms of database management,
+                    MongoDB has served as a reliable choice. Furthermore, the
+                    incorporation of various packages and libraries associated
+                    with these technologies has significantly augmented the
+                    sophistication and functionality of my projects. This
+                    holistic approach underscores a commitment to leveraging
+                    cutting-edge tools and frameworks, resulting in the creation
+                    of polished and feature-rich applications.
+                  </p>
+
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center overflow-hidden"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link className="projects" eventKey="first">
+                          React + Nextjs
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link className="projects" eventKey="second">
+                          MERN
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link className="projects" eventKey="third">
+                          HTML CSS JAVASCRIPT
+                        </Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projectsTab1.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {projectsTab2.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Cumque quam, quod neque provident velit, rem
+                          explicabo excepturi id illo molestiae blanditiis,
+                          eligendi dicta officiis asperiores delectus quasi
+                          inventore debitis quo.
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" alt="" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
