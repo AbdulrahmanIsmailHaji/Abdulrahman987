2313039ee3d50dcb82474c602c428968ab39489e
Abdulrahman Ismail <abdulrahmanismail987@gmail.com>
2024-02-01 11:45:31 +0300

    email sending

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,60 +1,35 @@
-import { useState } from "react";
+import React, { useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
+import emailjs from "@emailjs/browser";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Contact = () => {
-  const formInitialDetails = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    message: "",
-  };
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState("Send");
-  const [status, setStatus] = useState({});
+  const form = useRef();
 
-  const onFormUpdate = (category, value) => {
-    setFormDetails({
-      ...formDetails,
-      [category]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    setButtonText("Sending...");
 
-    try {
-      const response = await fetch("http://localhost:5000/contact", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+    emailjs
+      .sendForm(
+        "service_mtctmej",
+        "template_cjleeqc",
+        form.current,
+        "OH-oxL7vjgBOdtTYC"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          toast.success("Message sent successfully!"); // Display success notification
         },
-        body: JSON.stringify(formDetails),
-      });
-
-      const result = await response.json();
-
-      setButtonText("Send");
-
-      if (response.ok) {
-        setStatus({ success: true, message: "Message sent successfully" });
-      } else {
-        setStatus({
-          success: false,
-          message: "Something went wrong, please try again later.",
-        });
-      }
-    } catch (error) {
-      console.error("Error submitting form:", error);
-      setStatus({
-        success: false,
-        message: "Something went wrong, please try again later.",
-      });
-    }
+        (error) => {
+          console.log(error.text);
+          toast.error("Failed to send message."); // Display error notification
+        }
+      );
   };
 
   return (
@@ -83,72 +58,31 @@ export const Contact = () => {
                   }
                 >
                   <h2>Get In Touch</h2>
-                  <form onSubmit={handleSubmit}>
+                  <form ref={form} onSubmit={sendEmail}>
                     <Row>
                       <Col size={12} sm={6} className="px-1">
+                        <label htmlFor="user_name">Name</label>
                         <input
                           type="text"
-                          value={formDetails.firstName}
-                          placeholder="First Name"
-                          onChange={(e) =>
-                            onFormUpdate("firstName", e.target.value)
-                          }
+                          id="user_name"
+                          name="user_name"
+                          placeholder="Name"
                         />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
+                        <label htmlFor="user_email">Email</label>
                         <input
-                          type="text"
-                          value={formDetails.lasttName}
-                          placeholder="Last Name"
-                          onChange={(e) =>
-                            onFormUpdate("lastName", e.target.value)
-                          }
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
+                          id="user_email"
                           type="email"
-                          value={formDetails.email}
-                          placeholder="Email Address"
-                          onChange={(e) =>
-                            onFormUpdate("email", e.target.value)
-                          }
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="tel"
-                          value={formDetails.phone}
-                          placeholder="Phone No."
-                          onChange={(e) =>
-                            onFormUpdate("phone", e.target.value)
-                          }
+                          name="user_email"
+                          placeholder="Email"
                         />
                       </Col>
                       <Col size={12} className="px-1">
-                        <textarea
-                          rows="6"
-                          value={formDetails.message}
-                          placeholder="Message"
-                          onChange={(e) =>
-                            onFormUpdate("message", e.target.value)
-                          }
-                        ></textarea>
-                        <button type="submit">
-                          <span>{buttonText}</span>
-                        </button>
+                        <label>Message</label>
+                        <textarea name="message" />
+                        <input type="submit" value="Send" />
                       </Col>
-                      {status.message && (
-                        <Col>
-                          <p
-                            className={
-                              status.success === false ? "danger" : "success"
-                            }
-                          >
-                            {status.message}
-                          </p>
-                        </Col>
-                      )}
                     </Row>
                   </form>
                 </div>
@@ -157,6 +91,7 @@ export const Contact = () => {
           </Col>
         </Row>
       </Container>
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar />
     </section>
   );
 };
